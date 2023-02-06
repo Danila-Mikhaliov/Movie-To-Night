bb574bda6d4a58009753d27fa8b2553699e3f2c1
Aleksandr Keizerov <78848997+AaleksanderK@users.noreply.github.com>
2023-02-06 13:05:10 +0300

    fixed dropdown

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,28 +1,6 @@
 import '../styles/styles.scss'
+import { getDropdown } from './components/dropdown-menu.js'
 
-const dropdown = document.querySelector('.dropdown')
-const select = dropdown.querySelector('.dropdown__select')
-const caret = dropdown.querySelector('.select__caret')
-const menu = dropdown.querySelector('.dropdown__menu')
-const options = dropdown.querySelectorAll('.menu__item')
-const selected = dropdown.querySelector('.select__selected')
-
-select.addEventListener('click', () => {
-  select.classList.toggle('select-clicked')
-  caret.classList.toggle('select__caret-rotate')
-  menu.classList.toggle('dropdown__menu-open')
-})
-
-options.forEach((option) => {
-  option.addEventListener('click', () => {
-    selected.innerText = option.innerText
-    select.classList.remove('select-clicked')
-    caret.classList.remove('select__caret-rotate')
-    menu.classList.remove('dropdown__menu-open')
-  })
-
-  options.forEach((option) => {
-    option.classList.remove('active')
-  })
-  option.classList.add('active')
+window.addEventListener('DOMContentLoaded', () => {
+  getDropdown()
 })
