29279473623d8b6f959be5bf56467f41b0b39a72
Aleksandr Keizerov <78848997+AaleksanderK@users.noreply.github.com>
2023-02-08 21:44:02 +0300

    create card menu

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,3 +4,34 @@ import { getDropdown } from './components/dropdown-menu.js'
 window.addEventListener('DOMContentLoaded', () => {
   getDropdown()
 })
+
+const allCartMenu = document.querySelectorAll('.card__menu')
+allCartMenu.forEach((item) => {
+  item.addEventListener('click', createCardMenu)
+})
+
+function createCardMenu (event) {
+  const cardMenu = event.target.parentNode
+  const cardMenuActive = document.createElement('div')
+  cardMenuActive.classList.add('card__menu-active')
+
+  const btnAddDrop = document.createElement('button')
+  btnAddDrop.type = 'button'
+  btnAddDrop.classList = 'menu__add-drop menu__btn'
+  btnAddDrop.innerText = 'Добавить на доску'
+
+  const btnComplain = document.createElement('button')
+  btnComplain.type = 'button'
+  btnComplain.classList = 'menu__complain menu__btn'
+  btnComplain.innerText = 'Пожаловаться'
+
+  cardMenu.append(cardMenuActive)
+  cardMenuActive.append(btnAddDrop, btnComplain)
+  cardMenuActive.addEventListener('click', () => closeCardMenu(event, cardMenuActive))
+}
+function closeCardMenu (event, element) {
+  if (event.target === element.id) {
+    return
+  }
+  element.remove()
+}
