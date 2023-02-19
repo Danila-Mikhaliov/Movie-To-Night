ee8dd2bff2b43731fbda26e5a67162a61d71a0d2
danila_mikhaliov <danikmikhaliov200206@gmail.com>
2023-02-19 20:46:59 +0300

    add addition cards to desks in local storage

--- src/scripts/components/modal/openModal.js
+++ src/scripts/components/modal/openModal.js
@@ -5,8 +5,11 @@ export function openModal (e) {
   const modalOverlay = document.createElement('div')
   modalOverlay.classList.add('modal-overlay')
 
+  const parent = e.target.parentNode
+
   const modal = document.createElement('div')
   modal.classList.add('modal')
+  modal.id = parent.id
 
   modalOverlay.append(modal)
   document.body.appendChild(modalOverlay)
